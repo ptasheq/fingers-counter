b824d85cbdca3d634e6213420d1552683b45dae0
ptasheq <miecznik92@gmail.com>
2013-11-25 21:25:59 +0100

    partially image brightness test added

--- fingers_counter.py
+++ fingers_counter.py
@@ -1,12 +1,35 @@
+from __future__ import division
 import cv2, cv
 import numpy as np
 import time
+
+def getBrightness(frame, n):
+	tmp = np.vsplit(frame, n**0.5)
+	tmp2 = []
+	for j in [np.hsplit(i, n**0.5) for i in tmp]:
+		for k in j:
+			tmp2.append(np.average(k))
+	return tmp2
+
+class FirstFrame:
+	def __init__(self, frame, w, h):
+		self._width, self._height = w, h
+		self._frame = frame	
+		self._avgs = getBrightness(frame, 16)
+
+	def getImg(self):
+		return self._frame
+
+	def getAvgs(self):
+		return self._avgs
+
 
 class FingersCounter:
 	def __init__(self, cap):
 		cv2.namedWindow("Fingers Counter", cv2.CV_WINDOW_AUTOSIZE)
 		cv.SetMouseCallback("Fingers Counter", mouseCallback)
 		self._cap = cap
+		self._clock = 0;
 
 	def initCamera(self):
 		cameraReady = False
@@ -26,8 +49,9 @@ class FingersCounter:
 				time.sleep(3)
 			retval, frame = cap.read()
 
-		self._firstFrame = frame
-
+		frame = cv2.cvtColor(frame, cv.CV_RGB2GRAY)	
+		self._firstFrame = FirstFrame(frame, 640, 480) 
+		self._lastAvgs = self._firstFrame.getAvgs()
 
 	def run(self):
 		self.initCamera()
@@ -36,7 +60,18 @@ class FingersCounter:
 
 	def display(self):
 		retval, frame = cap.read()
-		cv2.imshow("Fingers Counter", cv2.absdiff(self._firstFrame, frame))
+		frame = cv2.cvtColor(frame, cv.CV_RGB2GRAY)	
+
+		img = cv2.absdiff(frame, self._firstFrame.getImg())
+
+		retval, thresh = cv2.threshold(img, 30, 100, cv2.THRESH_BINARY)
+		if time.clock() - self._clock > 0.25:
+			self._clock = time.clock()
+			tmp = getBrightness(frame, 16) 
+			'''for x, y in zip(tmp, self._lastAvgs):
+				TODO: insert here brightness calibration'''
+			self._lastAvgs = tmp
+		cv2.imshow("Fingers Counter", thresh)
 		return cv2.waitKey(30)
 
 def mouseCallback(event, x, y, a, b):
