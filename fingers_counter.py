d4799e38f5a0bb5ba53a46db446953b92442a2ee
ptasheq <miecznik92@gmail.com>
2013-11-26 15:04:16 +0100

    partially image brightness test added v2

--- fingers_counter.py
+++ fingers_counter.py
@@ -1,6 +1,7 @@
 from __future__ import division
-import cv2, cv
+import cv2, cv 
 import numpy as np
+from math import ceil
 import time
 
 def getBrightness(frame, n):
@@ -11,11 +12,36 @@ def getBrightness(frame, n):
 			tmp2.append(np.average(k))
 	return tmp2
 
+def getBrightnessChange(lastFrameAvgs, newFrame, parts):
+	tmp = getBrightness(newFrame, parts) 
+
+	# a is number of brightened rectangles, b is number of darkened rectangles
+	a, b = 0,0; minxy, minyx = 1000, 1000
+	for x, y in zip(tmp, lastFrameAvgs):
+		if x > 1.04*y:
+			a += 1
+		elif 1.04*x < y:
+			b += 1
+		if minxy > x - y and x - y > 0:
+			minxy = x - y
+		if minyx > y - x and y - x > 0:
+			minyx = y - x
+	val = 0
+
+	#if we image is generally brightened we want to brighten first frame also
+	print(minxy, minyx)
+	if a > parts / 2: 
+		val = ceil(minxy)
+	elif b > parts / 2:
+		val = -ceil(minyx)
+
+	return tmp, int(val)
+
 class FirstFrame:
-	def __init__(self, frame, w, h):
+	def __init__(self, frame, w, h, parts):
 		self._width, self._height = w, h
 		self._frame = frame	
-		self._avgs = getBrightness(frame, 16)
+		self._avgs = getBrightness(frame, parts)
 
 	def getImg(self):
 		return self._frame
@@ -23,13 +49,22 @@ class FirstFrame:
 	def getAvgs(self):
 		return self._avgs
 
+	def adjustBrightness(self, val):
+		if val != 0:
+			problem is that we can get too low or too high values
+			'''x = np.add(self._frame, val)
+			print(x.shape)
+			self._frame = x'''
+
+
 
 class FingersCounter:
 	def __init__(self, cap):
 		cv2.namedWindow("Fingers Counter", cv2.CV_WINDOW_AUTOSIZE)
 		cv.SetMouseCallback("Fingers Counter", mouseCallback)
 		self._cap = cap
-		self._clock = 0;
+		self._clock = 0
+		self._parts = 16
 
 	def initCamera(self):
 		cameraReady = False
@@ -50,7 +85,7 @@ class FingersCounter:
 			retval, frame = cap.read()
 
 		frame = cv2.cvtColor(frame, cv.CV_RGB2GRAY)	
-		self._firstFrame = FirstFrame(frame, 640, 480) 
+		self._firstFrame = FirstFrame(frame, 640, 480, self._parts) 
 		self._lastAvgs = self._firstFrame.getAvgs()
 
 	def run(self):
@@ -64,13 +99,13 @@ class FingersCounter:
 
 		img = cv2.absdiff(frame, self._firstFrame.getImg())
 
-		retval, thresh = cv2.threshold(img, 30, 100, cv2.THRESH_BINARY)
-		if time.clock() - self._clock > 0.25:
+		if time.clock() - self._clock > 0.2:
 			self._clock = time.clock()
-			tmp = getBrightness(frame, 16) 
-			'''for x, y in zip(tmp, self._lastAvgs):
-				TODO: insert here brightness calibration'''
-			self._lastAvgs = tmp
+			self._lastAvgs, val = getBrightnessChange(self._lastAvgs,
+			                                          frame, self._parts)
+			self._firstFrame.adjustBrightness(val)
+
+		retval, thresh = cv2.threshold(img, 30, 100, cv2.THRESH_BINARY)
 		cv2.imshow("Fingers Counter", thresh)
 		return cv2.waitKey(30)
 
